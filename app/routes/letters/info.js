442bd176328519924f8702796c12a6a91ff030f4
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-04-13 07:44:20 +0200

    remove formatting of content

--- app/routes/letters/info.js
+++ app/routes/letters/info.js
@@ -5,7 +5,6 @@ import { set } from '@ember/object';
 export default Route.extend(AuthenticatedRouteMixin, {
 
   async afterModel(model) {
-    set(model, "content", model.content.replace(/\n/g, "<br />"))
     set(model, "newLabels", model.label);
     const labels = await this.store.findAll('label');
     set(model, "labels", []);
