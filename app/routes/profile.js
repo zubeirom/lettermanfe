d543a9df0bb857eb6ad4b928ccfc013124f100ea
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-04-17 02:57:16 +0200

    Add bottom menu and little ui fixes

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin,{
+  session: service(),
+
+  afterModel() {
+    console.log(this.session.data.authenticated.user);
+  }
 });
