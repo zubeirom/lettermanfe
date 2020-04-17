588cc4e54d040bad245e6f31651554694335fa51
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-04-17 06:00:21 +0200

    disabling buttons when login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import firebase from 'firebase/app';
+import { set } from '@ember/object';
 
 export default Controller.extend({
   session: service(),
@@ -18,15 +19,18 @@ export default Controller.extend({
   actions: {
     async authGoogle() {
       try {
+        set(this, "loading", true);
         const auth = await this.get('firebaseApp').auth();
         const provider = new firebase.auth.GoogleAuthProvider();
         return auth.signInWithPopup(provider).then(() => {
           document.location.reload();
         }).catch(e => {
+          set(this, "loading", false);
           console.error(e);
           this.checkIfAlreadyUsedEmail(e.code)
         });
       } catch (error) {
+        set(this, "loading", false);
         console.error(error);
         this.toast.error('Something went wrong', 'Error');
       }
@@ -34,15 +38,18 @@ export default Controller.extend({
 
     async authFacebook() {
       try {
+        set(this, "loading", true);
         const auth = await this.get('firebaseApp').auth();
         const provider = new firebase.auth.FacebookAuthProvider();
         return auth.signInWithPopup(provider).then(() => {
           document.location.reload();
         }).catch(e => {
+          set(this, "loading", false);
           console.error(e);
           this.checkIfAlreadyUsedEmail(e.code)
         });
       } catch (error) {
+        set(this, "loading", false);
         this.toast.error('Something went wrong', 'Error');
         console.error(error);
       }
@@ -50,15 +57,18 @@ export default Controller.extend({
 
     async authTwitter() {
       try {
+        set(this, "loading", true);
         const auth = await this.get('firebaseApp').auth();
         const provider = new firebase.auth.TwitterAuthProvider();
         return auth.signInWithPopup(provider).then(() => {
           document.location.reload();
         }).catch(e => {
+          set(this, "loading", false);
           console.error(e);
           this.checkIfAlreadyUsedEmail(e.code)
         });
       } catch (error) {
+        set(this, "loading", false);
         this.toast.error('Something went wrong', 'Error');
         console.error(error);
       }
@@ -66,15 +76,18 @@ export default Controller.extend({
 
     async authGithub() {
       try {
+        set(this, "loading", true);
         const auth = await this.get('firebaseApp').auth();
         const provider = new firebase.auth.GithubAuthProvider();
         return auth.signInWithPopup(provider).then(() => {
           document.location.reload();
         }).catch(e => {
+          set(this, "loading", false);
           console.error(e);
           this.checkIfAlreadyUsedEmail(e.code)
         });
       } catch (error) {
+        set(this, "loading", false);
         this.toast.error('Something went wrong', 'Error');
         console.error(error);
       }
@@ -82,15 +95,18 @@ export default Controller.extend({
 
     async authMicrosoft() {
       try {
+        set(this, "loading", true);
         const auth = await this.get('firebaseApp').auth();
         const provider = new firebase.auth.OAuthProvider('microsoft.com');
         return auth.signInWithPopup(provider).then(() => {
           document.location.reload();
         }).catch(e => {
+          set(this, "loading", false);
           console.error(e);
           this.checkIfAlreadyUsedEmail(e.code)
         });
       } catch (error) {
+        set(this, "loading", false);
         this.toast.error('Something went wrong', 'Error');
         console.error(error);
       }
